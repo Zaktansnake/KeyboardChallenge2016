302df2a3f3f87ef8d625bd6bee08cd970974931c
Zaktansnake <david_chongym@outlook.com>
2016-01-24 00:54:32 +0800

    Created randomizer for mindblown

--- src/Mindblown.java
+++ src/Mindblown.java
@@ -1,4 +1,19 @@
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.util.Random;
+
+import com.sun.glass.events.KeyEvent;
 
 public class Mindblown {
-
+	
+	public static void process(int keyEvent) {
+		try {
+			Robot robot = new Robot();
+			Random random = new Random();
+			int randomizedNumber = random.nextInt(90);
+			robot.keyPress(randomizedNumber);
+		} catch(AWTException e) {
+			e.printStackTrace();
+		}
+	}
 }
